0ceb46e5c6a79bcea10e90e71992f562c515350a
orangina92 <user@USER-no-MacBook-Pro.local>
2019-03-18 19:04:23 +0900

    edit-search.js
    クラス名の修正、変数の修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,24 +1,19 @@
 $(function() {
-  var user_list = $("#user_search_result");
-
-  var member_list = $("#member_search_result");
-
-  function appendUsers(user) {
+ function appendUsers(user) {
    var html = `<div class="chat-group-user clearfix">
    <p class="chat-group-user__name">${user.name}</p>
    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
    </div>`
-   user_list.append(html);
+   $("#user-search-result").append(html);
  }
 
- function appendMembers(name, user_id) {
+ function appendMembers(name,user_id) {
   var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
   <input name='group[user_ids][]' type='hidden' value='${user_id}'>
   <p class='chat-group-user__name'>${name}</p>
   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
   </div>`
-
-  member_list.append(html);
+  $("#chat-group-users").append(html);
 }
 
 function appendErrMsgToHTML(msg) {
@@ -30,7 +25,6 @@ function appendErrMsgToHTML(msg) {
 
 $("#user-search-field").on("keyup", function() {
   var input = $("#user-search-field").val();
-  console.log('input');
   $.ajax({
     type: 'GET',
     url: '/users',
@@ -39,7 +33,7 @@ $("#user-search-field").on("keyup", function() {
   })
 
   .done(function(members) {
-   $("#user_search_result").empty();
+   $("#user-search-result").empty();
    if (members.length !== 0) {
      members.forEach(function(user){
        appendUsers(user);
@@ -53,17 +47,18 @@ $("#user-search-field").on("keyup", function() {
     alert('ユーザー検索に失敗しました');
   })
 });
-});
+
 
 $(function() {
-  $(document).on("click", '.user_search_add', function() {
+  $(document).on("click", '.user-search-add', function() {
     var name = $(this).attr("data-user-name");
     var user_id = $(this).attr("data-user-id");
     $(this).parent().remove();
-    appendMembers(name, user_id);
+    appendMembers(name,user_id);
   });
-  $(document).on("click", '.user_search_remove', function() {
+  $(document).on("click", '.user-search-remove', function() {
     $(this).parent().remove();
   });
+});
 });
 
