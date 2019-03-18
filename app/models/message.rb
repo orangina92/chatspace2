f2bd2cefe2778af7950ee531e6a4b761c4e68801
orangina92 <user@USER-no-MacBook-Pro.local>
2019-03-18 19:40:48 +0900

    edit-models/message.rb
    validatesの表記を変更した。

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,16 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :content, presence: true, unless: :image?
-  mount_uploader :image, ImageUploader
+  validates :group_id, :user_id, presence: true, numericality: {only_integer: true}
+  validates :body_or_image, presence: true
+
+  mount_uploader :image, Message::ImageUploader
+
+  private
+
+  def body_or_image
+    body.presence or image.presence
+  end
 end
 
 
