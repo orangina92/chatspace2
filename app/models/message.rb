902f139bcde8387e3074a1182ffb752205018e9e
orangina92 <user@USER-no-MacBook-Pro.local>
2019-03-18 22:22:10 +0900

    some-fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,14 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :group_id, :user_id, presence: true, numericality: {only_integer: true}
-  validates_presence_of :content, :unless => :image?
-  validates_presence_of :image, :unless => :content?
+  validates :body_or_image, presence: true
   mount_uploader :image, Message::ImageUploader
+  private
+
+  def body_or_image
+    body.presence or image.presence
+  end
+
 
 end
 
